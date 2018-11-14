e54caa3aefdef0a65fee6614d7349e6989e3e3c7
George Benjamin <msdcconnect@gmail.com>
2018-11-14 16:13:21 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const http = require('http');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.set("views", path.resolve(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.static(path.resolve(__dirname, "public")));
+
+//Global variables
+app.locals.author = "George Benjamin";
+app.locals.email = "msdcconnect@gmail.com"
+
+app.get("/", (req, res) =>{
+    res.render("index", {
+        steps: ["This is my awesome express app",
+        "This is my awesome express app",
+        "This is my awesome express app",],
+        title:"How to build an octocat"
+    })
+});
+
+app.get("/about", (req, res) =>{
+    res.render("about", {
+        title:"Let me tell you a little bit about Express",
+        features:[
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit,"
+        ]
+    })
+})
+
+app.get("/contact", (req, res) =>{
+    res.render("contact", {
+        title:"Wanna talk? Send a mail!"
+    });
+})
+
+app.use((req, res) =>{
+    res.send("Page not found");
+});
+
+http.createServer(app).listen(3000);
